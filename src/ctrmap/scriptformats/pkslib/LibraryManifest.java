3e40c88250dd78c7333e0689edcfbda2ae860872
PlatinumMaster <L33TGamer@outlook.com>
2026-04-13 00:00:59 -0400

    Add script editor support stubs and IDE helper methods
    Add ScriptOpenMode enum, BeaterScript assembler stub, LibraryManifest
    constant, and CTRMapIDEHelper methods (setIDE, getCommandDBIndices,
    getScriptByProject) needed by CTRMapV script editor.

---
+++ src/ctrmap/scriptformats/pkslib/LibraryManifest.java
@@ -0,0 +1,9 @@
+package ctrmap.scriptformats.pkslib;
+
+/**
+ * Manifest for PokeScript include libraries.
+ */
+public class LibraryManifest {
+
+	public static final String LIBRARY_MANIFEST_NAME = "manifest.yml";
+}
